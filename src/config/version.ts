1402f91666ece4945fa86c7402dcdf14adb9b6a4
seokhyu0405 <sanghyuyoon@gmail.com>
2026-01-04 20:51:30 +0900

    chore: 캐시 초기화를 위한 버전 업데이트

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-01-04T07:18:33.527Z
+// Build Time: 2026-01-04T11:51:22.044Z
 export const APP_VERSION = {
-  version: '1767511113527',
-  buildTime: '2026-01-04T07:18:33.527Z',
-  hash: 'mt0l47a5ll'
+  version: '1767527482044',
+  buildTime: '2026-01-04T11:51:22.044Z',
+  hash: 'zfu3ociq87k'
 };
 
 export default APP_VERSION;
