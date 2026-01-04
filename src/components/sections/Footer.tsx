e0c185195b41e42717e81570b21328def28c9c95
seokhyu0405 <sanghyuyoon@gmail.com>
2026-01-04 21:12:38 +0900

    update: 갤러리 이미지 및 Footer 수정

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
       {/* WeddingInvitation-Footer-NonCommercial DO NOT CHANGE*/}
       <FooterContent>
         <Copyright>Wonseok and Sanghyu</Copyright>
-        <Credits>Since 20230409<br />Made with love</Credits>
+        <Credits>Made with love</Credits>
         <HiddenAttribution data-jwk-id={watermarkId}>
           NonCommercial
         </HiddenAttribution>
